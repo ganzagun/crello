75d3529307618d9775877026c07913e05afdaf84
Sarthak Malik <maliksarthak003@gmail.com>
2021-04-20 18:58:21 +0530

    Data added

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,12 +1,25 @@
+const cards = [
+    {
+        id: 'card-1',
+        title: 'card-1',
+    },
+
+];
 
 const data = {
     lists: {
         'list-1': {
             id: 'list-1',
             title: 'Todo',
+            cards,
+        },
+        'list-2': {
+            id: 'list-2',
+            title: 'Doing',
+            cards: [],
         },
     },
-    listIds: ['list-1'],
+    listIds: ['list-1', 'list-2'],
 };
 
 export default data;
